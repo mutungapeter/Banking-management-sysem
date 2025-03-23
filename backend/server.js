53f3b933cbbac7b8d23c6625ae7a8e67e4cd88bb
Peter Mutunga <petersiku99@gmail.com>
2025-03-23 11:58:30 +0300

    modified the cors origin to be loaded from .env

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
